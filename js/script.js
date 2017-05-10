b7d48b265c2eedbb04c193998794542decc3a5b9
shariff6 <shariff.mohammed.a@gmail.com>
2017-05-10 16:28:36 +0300

    beautifued page using atom beautify'

--- js/script.js
+++ js/script.js
@@ -1,18 +1,25 @@
 function Tasks(task, description) {
   this.inputTask = task
-  this.inputDescripion = description
+  this.inputDescription = description
 }
 Tasks.prototype.taskName = function() {
-  return this.inputTask + " " + this.inputDescripion;
+  return this.inputTask;
 }
 
 $(document).ready(function() {
-      $("form#tasks").submit(function(event) {
-            event.preventDefault();
-            var inputtedTask = $("input#importantTask").val();
-            var inputtedDescription = $("textarea#comment").val();
+  $("form#tasks").submit(function(event) {
+    event.preventDefault();
+    var inputtedTask = $("input#importantTask").val();
+    var inputtedDescription = $("textarea#comment").val();
 
-            var newTask = new Tasks(inputtedTask, inputtedDescription);
-            $("ul#tasks").append("<li>" + newTask.taskName() + "</li>");
-          })
-        })
+    var newTask = new Tasks(inputtedTask, inputtedDescription);
+    $("ul#tasks").append("<li><span class = 'describe'>" + newTask.taskName() + "</span></li>");
+    $("input#importantTask").val();
+    $("textarea#comment").val();
+    $(".describe").click(function() {
+      $("#show-description").last().toggle();
+      $("#show-description h2").text(newTask.inputTask);
+      $(".task-description").text(newTask.inputDescription);
+    })
+  })
+})
